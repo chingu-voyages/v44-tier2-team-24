fcec54598551b2fc2706ef17b07528c88e6ee445
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-05-13 19:22:59 -0400

    feat/modal added sweetalerts2 to give user instructins on button click

--- boolebot/src/Pages/HomePage.js
+++ boolebot/src/Pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./HomePage.module.css";
 import Container from "../Components/Layout/Container";
+import Swal from "sweetalert2";
 
 export default function Homepage() {
   return (
@@ -9,7 +10,21 @@ export default function Homepage() {
         <h1 className={styles.title}>BooleBots</h1>
         <section className={styles.homepageBtns}>
           <button className={styles.startBtn}>Start</button>
-          <button className={styles.instructionBtn}>How to play</button>
+          <button
+            className={styles.instructionBtn}
+            onClick={() => {
+              Swal.fire({
+                title: "We will explain how to play the game",
+                width: 600,
+                padding: "3em",
+                color: "#716add",
+                background:
+                  "#fff url(https://sweetalert2.github.io/#iconsimages/trees.png)",
+              });
+            }}
+          >
+            How to play
+          </button>
         </section>
       </Container>
     </div>
