e14ade4f79340692a94cf5fb44572ef0485ec11a
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-05-13 14:32:54 -0400

    feat/added starting point for Homepage

--- boolebot/src/App.js
+++ boolebot/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Homepage from "./Pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h2>Boole Bot</h2>
+      <Homepage />
     </div>
   );
 }
---
+++ boolebot/src/Pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+
+export default function Homepage() {
+  return (
+    <div className={styles.main_body}>
+      <h1 className={styles.title}>BooleBots</h1>
+    </div>
+  );
+}
