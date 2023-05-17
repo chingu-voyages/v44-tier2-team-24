cc9e5eef04cdd22e29cd3deac50308ea2425360a
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-05-17 19:14:26 -0400

    applied sass styles to botInfo page

--- boolebot/src/Pages/BotsInfo.jsx
+++ boolebot/src/Pages/BotsInfo.jsx
@@ -3,25 +3,29 @@ export default function BotsInfo() {
   return (
 <>
 <h2>Create Bot</h2>
+<div className="createdBots">
+  <h3>bot 1</h3>
+  <h3>bot 2</h3>
+</div>
 <div className="test">
   <form >
     <fieldset >
       
       <label htmlFor="bot_name">
-        Name your bot
+        Name your bot:
 
         <input type="text" id="bot_name" name="bot_name"/>
       </label>
       <div></div>
       <label htmlFor="boole_value">
-        Choose a Boolean Value
+        Choose a Boolean Value:
         <select id="boole_value" name="boole_value">
           <option value="1">1</option>
           <option value="0">0</option>
         </select>
       </label>
       <label htmlFor="boole_op">
-        Choose Boolean Operator
+        Choose Boolean Operator:
         <select id="boole_op" name="boole_op">
           <option value="and">AND</option>
           <option value="or">OR</option>
@@ -30,11 +34,11 @@ export default function BotsInfo() {
         </select>
       </label>
       <label htmlFor="bot_speed">
-        Choose Speed
+        Choose Speed:
         <input id="bot_speed" type="range" min={0} max={100} />
       </label>
       <label htmlFor="bot_direction">
-        Bot Direction
+        Bot Direction:
         <select>
           <option value="north">NORTH</option>
           <option value="south">SOUTH</option>
