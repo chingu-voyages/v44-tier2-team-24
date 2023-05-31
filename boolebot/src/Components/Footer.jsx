6828095ee5f553e7e69d12688ff02716b3079828
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-05-31 15:29:02 -0400

    Added Links on the footer with Creator's names

--- boolebot/src/Components/Footer.jsx
+++ boolebot/src/Components/Footer.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import styles from "./Footer.module.css";
+// import styles from "./Footer.module.css";
+import { Link } from "react-router-dom";
 import Container from "./Layout/Container";
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
+    <div className='footer'>
    
-        <a
-          className={styles.footer_link}
+        <Link
+          className='footer_link'
           href="https://github.com/chingu-voyages/v44-tier2-team-24/tree/main"
           target="_blank"
           rel="noreferrer"
         >
           <i className="fa fa-github"></i>
-        </a>
-      
+        </Link>
+        <ul>
+          <li><Link to="/">Hector</Link></li>
+          <li><Link to="/">Jack</Link></li>
+          <li><Link to="/">Libby</Link></li>
+          <li><Link to="/">Sucheta</Link></li>
+        </ul>
+       
     </div>
   );
 }
