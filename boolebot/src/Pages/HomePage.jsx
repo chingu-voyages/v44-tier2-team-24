5fdaf72faf5f97aca87e62a1c67fcf1942b83813
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2023-06-01 16:12:09 -0400

    Added useEffect hook to invoke sweetAlert and changed inside text content/ message

--- boolebot/src/Pages/HomePage.jsx
+++ boolebot/src/Pages/HomePage.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Swal from "sweetalert2";
 import sweetAlertMixin from "../Components/SweetAlertConfig";
 import { Link } from "react-router-dom";
 import botImage from "../assets/booleBotsHome.png";
 
-export default function Homepage() {
-  
 
-  
+export default function Homepage() {
+  // const [isLoading, setIsLoading] = useState(true);
   const showInstructions = () => {
     sweetAlertMixin.fire({
-      title: "We will explain how to play the game",
+      title: 'BOOLBOTS',
+      text: "Explore the fascinating world of boolean values through an entertaining game called Boolebots. Select a board size, assemble your own army of bots, and witness the magic of boolean operators in action. Let's dive in and unravel the mysteries of these operators together!"
     });
   };
+  useEffect(()=>{
+    showInstructions();
+  }, [])
+  
 
   return (
     <div className="main_body">
@@ -26,10 +30,10 @@ export default function Homepage() {
           <button className="startBtn">Start</button>
         </Link>
 
-        {/* Use the showInstructions function as the click event handler */}
+        {/* Use the showInstructions function as the click event handler
         <button className="instructionBtn" onClick={showInstructions}>
           How to play
-        </button>
+        </button> */}
       </section>
     </div>
   );
