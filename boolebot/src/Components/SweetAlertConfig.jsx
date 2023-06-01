3a95ca25c6634d745cdfcedc251883e4026f7fcd
libbyReeves <libby.reeves@hotmail.com>
2023-06-01 16:18:24 -0400

    added sweetalert partials page and connected it to the sweetalert component

--- boolebot/src/Components/SweetAlertConfig.jsx
+++ boolebot/src/Components/SweetAlertConfig.jsx
@@ -1,10 +1,29 @@
+import "../SASS/Partials/_sweetalertbutton.scss";
 import Swal from 'sweetalert2';
 
 const sweetAlertMixin = Swal.mixin({
     width: 600,
   padding: '3em',
-  color: '#716add',
-  background: '#fff url(https://sweetalert2.github.io/#iconsimages/trees.png)',
+  color: '#061d41',
+  background: '#d3f3ee',
+  confirmButtonColor: '#061d41',
+  customClass: {
+    confirmButton: 'swal2-confirm',
+  }
 });
 
 export default sweetAlertMixin;
+
+
+
+/* for having multiple button options
+}).then((result) => {
+  if (result.isConfirmed) {
+    // OK button was clicked
+    console.log('OK button clicked');
+  } else if (result.isDismissed) {
+    // Cancel button was clicked
+    console.log('Cancel button clicked');
+  }
+});
+*/
