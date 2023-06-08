d8dc777fb608ed4520fb93974c46a9a6bec48944
libbyReeves <libby.reeves@hotmail.com>
2023-06-08 17:46:48 -0400

    debug-fixed arena gameplay issue with the new styling

--- boolebot/src/utils/collisionLogic.jsx
+++ boolebot/src/utils/collisionLogic.jsx
@@ -7,8 +7,6 @@ const checkCollision = (botsArr) => {
   //write logic for collision
   const locationArr = botsArr.map((bot) => bot.position);
 
-  console.log("Location array", locationArr);
-
   for (let i = 0; i < locationArr.length; i++) {
     for (let j = i + 1; j < locationArr.length; j++) {
       if (locationArr[i] === locationArr[j]) {
@@ -20,9 +18,7 @@ const checkCollision = (botsArr) => {
   return null;
 };
 
-const handleCollision = (botsArr, operator, currBotName, setMessage) => {
-  console.log("COLLISION!!!!!!!!!!");
-
+const handleCollision = (botsArr, operator, currBotName, setMessage) => {  
   const positionMap = new Map();
 
   botsArr.forEach((bot) => {
@@ -33,15 +29,9 @@ const handleCollision = (botsArr, operator, currBotName, setMessage) => {
     }
   });
 
-  console.log("MAP ", positionMap);
-
-  //will hold the positions that contain more than 1 bot
   let colidedPosition;
-
-  //will hold an array of bots that have collided
   const colidedBots = ["", ""];
 
-  //only get the positions where there is more than 1 bots
   for (const [key, value] of positionMap.entries()) {
     if (value > 1) {
       colidedPosition = key;
@@ -49,18 +39,8 @@ const handleCollision = (botsArr, operator, currBotName, setMessage) => {
     }
   }
 
-  console.log("CurrBotNAme ", currBotName);
-
-  //find all bots with matching collision position
   for (let i = 0; i < botsArr.length; i++) {
     if (botsArr[i].position == colidedPosition) {
-      console.log(
-        "CurrBotNAme ",
-        currBotName,
-        "BotBeing looked at",
-        botsArr[i].name
-      );
-
       if (currBotName === botsArr[i].name) {
         colidedBots[0] = botsArr[i];
       } else {
@@ -69,37 +49,24 @@ const handleCollision = (botsArr, operator, currBotName, setMessage) => {
     }
   }
 
-  console.log("THESE ARE THE BOTS THAT SHOULD BATTLE ", colidedBots);
-
-  // determine winer & loser
-  // using AND
-
   switch (operator) {
     case "AND":
       const AND_Result = colidedBots[0].value && colidedBots[1].value;
       return updateScore(AND_Result, colidedBots[0], colidedBots[1], setMessage);
-      // refactor the score-updating logic to use setter function instead of mutating array by reference
-      break;
     case "OR":
       const OR_Result = colidedBots[0].value || colidedBots[1].value;
       return updateScore(OR_Result, colidedBots[0], colidedBots[1], setMessage);
-      break;
     case "XOR":
       const XOR_Result = colidedBots[0].value ^ colidedBots[1].value;
-      return updateScore(XOR_Result, colidedBots[0], colidedBots[1]), setMessage;
-      break;
+      return updateScore(XOR_Result, colidedBots[0], colidedBots[1], setMessage);
     case "NOR":
       const NOR_Result = !(colidedBots[0].value || colidedBots[1].value);
-      return updateScore(NOR_Result, colidedBots[0], colidedBots[1],setMessage);
-      break;
+      return updateScore(NOR_Result, colidedBots[0], colidedBots[1], setMessage);
   }
 };
 
 const updateScore = (result, botOne, botTwo, setMessage) => {
   if (result) {
-    // botOne.wins = botOne.wins + 1
-    // botTwo.loses = botTwo.loses + 1
-
     const botOneClone = new BotClass(
       botOne.position,
       botOne.direction,
@@ -120,10 +87,10 @@ const updateScore = (result, botOne, botTwo, setMessage) => {
       botTwo.loses + 1,
     );
 
-    setMessage("COLLISION!!!")
+    setMessage("COLLISION!!!");
     return [botOneClone, botTwoClone];
   } else {
-    setMessage("TIE!!!")
+    setMessage("TIE!!!");
     return;
   }
 };
