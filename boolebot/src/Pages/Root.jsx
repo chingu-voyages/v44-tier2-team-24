84f87094ef335259b31aae1930cd6e3fad2a2b5d
libbyReeves <libby.reeves@hotmail.com>
2023-06-11 17:37:07 -0400

    removed Hamburger menu

--- boolebot/src/Pages/Root.jsx
+++ boolebot/src/Pages/Root.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navigation from '../Components/Navigation';
 import Footer from '../Components/Footer';
-import Hamburger from '../Components/Hamburger';
 
 function RootLayout() {
     return (
